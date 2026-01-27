652944e1b9a29a28ee599fb29b3e0416c2c187ea
v_jiajschen <v_jiajschen@tencent.com>
2026-01-27 14:52:37 +0800

    add auto update self

--- build.cpp
+++ build.cpp
@@ -2,13 +2,16 @@
 
 int main(int argc, char** argv) {
     using namespace csc;
-    Cmd cmd;
-    try {
-        cmd.Append("clang++", "-o", "main", "main.cpp");
-        csc::run_cmd(cmd);
+    using std::filesystem::path;
 
-    } catch (const std::exception& e) {
-        csc_log(CSC_ERRO, e.what());
-        return 1;
+    auto result = build::register_rebuild_self(argc, argv, "build.cpp");
+    if (!result) {
+        csc_log(CSC_ERRO, result.error().message);
+    } else {
+        if (result.value()) return 0;
     }
+    Cmd cmd;
+    cmd.Append("clang++", "-o", "main", "main.cpp");
+    csc::run_cmd(cmd);
+    csc_log(CSC_INFO, "have success build target!");
 }
