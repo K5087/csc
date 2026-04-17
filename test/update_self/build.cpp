da1afbd52484149f469a2693d87d711d40321ba3
moke <18336485360@163.com>
2026-04-17 14:21:01 +0800

    remove useless function

--- test/update_self/build.cpp
+++ test/update_self/build.cpp
@@ -5,9 +5,5 @@ using namespace csc;
 int main(int argc, char** argv) {
     update_self(argc, argv, __FILE__, {"../../csc.hpp"});
 
-    Cmd cmd("clang++", "-o", "main", "main.cpp");
-    cmd.Append("-std=c++23");
-    run_cmd(cmd);
-
-    // log(INFO, "have success build target!");
+    CMD("clang++", "-o", "main", "main.cpp", "-std=c++23");
 }
